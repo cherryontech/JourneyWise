029a4df326ad0248ff99bb7ed8c9aacaab570c9b
Mahi <Mahi@Mahis-MBP.lan>
2024-04-29 22:44:23 +0200

    implimented feature button for https://marisolsaumi.atlassian.net/browse/JW-59 & https://marisolsaumi.atlassian.net/browse/JW-59. Added tailwnidn , updated the enlist file , added the landingpage and updated prettier

---
+++ journeywise-app/prettier.config.cjs
@@ -0,0 +1,9 @@
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  trailingComma: 'es5',
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+}
+
+export default config
--- journeywise-app/src/App.jsx
+++ journeywise-app/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './App.css'
+import Buttons from './components/Buttons/Buttons'
 
 function App() {
   return (
     <>
-      <div>Welcome to JourneyWise by VenusVoyager </div>
+      <div className="text-center text-xl font-semibold text-yellow-500 ">
+        Welcome to JourneyWise by VenusVoyagers
+      </div>
+
+      <Buttons primary rounded>
+        Button
+      </Buttons>
+      <Buttons primary className={'text-green-500'}>
+        Hello
+      </Buttons>
     </>
   )
 }
