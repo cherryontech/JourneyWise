1257a044d1c4181dc8d88678bac646ef6ab0e259
bpb2008 <brittanythompson08@gmail.com>
2024-04-30 22:25:42 -0500

    restarted to fix some package.json error
    resolving conflicts

---
+++ journeywise-app/src/navigation/Index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div id="heading" className="p-0 mt-80">
+      <div id="h2heading" className="w-[558px]">
+        <h2 className="text-6xl">Finding Your Path & Story</h2>
+      </div>
+      <div className="subheading">
+        <h6>
+          During this time of uncertainty, lets redefine how you tell
+          your story.
+        </h6>
+      </div>
+    </div>
+  )
+}
+
+export default Index
