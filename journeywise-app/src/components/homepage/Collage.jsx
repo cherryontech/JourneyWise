807dbe54dcb2bd0667528abb61076e8b8380f54a
bpb2008 <brittanythompson08@gmail.com>
2024-05-01 21:24:34 -0500

    committing so I can add Mahlet's button

--- journeywise-app/src/components/homepage/Collage.jsx
+++ journeywise-app/src/components/homepage/Collage.jsx
@@ -4,31 +4,41 @@ const Collage = () => {
   return (
     <div
       id="collage-container"
-      className="grid grid-cols-3 grid-rows-3 gap-0"
+      className="grid grid-cols-3 grid-rows-3 gap-0 border mt-[-126px]"
     >
+      <div id="grid-item" className="shrink-0 w-[237px] h-[254px]">
+        pic 1
+      </div>
+      <div id="grid-item" className="shrink-0 w-[237px] h-[254px]">
+        <img src=".collage/Union.svg" />
+      </div>
+      <div id="grid-item" className="shrink-0 w-[237px] h-[254px]">
+        pic 3
+      </div>
+      <div id="grid-item" className="shrink-0 w-[237px] h-[254px]">
+        pic 4
+      </div>
+      <div id="grid-item" className="shrink-0 w-[237px] h-[254px]">
+        pic 5
+      </div>
+      <div id="grid-item" className="shrink-0 w-[237px] h-[254px]">
+        pic 6
+      </div>
       <div
         id="grid-item"
         className="shrink-0 w-[234.886px] h-[251.828px]"
       >
-        <svg
-          width="235"
-          height="253"
-          viewBox="0 0 235 253"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-        />
+        pic 7
       </div>
-      <div id="grid-item">
-        <img src="./assets/homepage/collage/Union.svg" />
+      <div
+        id="grid-item"
+        className="shrink-0 w-[234.886px] h-[251.828px]"
+      >
+        pic 8
       </div>
-      <div id="grid-item"></div>
-      <div id="grid-item"></div>
-      <div id="grid-item"></div>
-      <div id="grid-item"></div>
-      <div id="grid-item"></div>
-      <div id="grid-item"></div>
-      <div id="grid-item"></div>
+      <div id="grid-item" className="shrink-0 w-[237px] h-[254px]">
+        pic 9
+      </div>
     </div>
   )
 }
