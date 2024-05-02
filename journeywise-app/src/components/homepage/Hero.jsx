aaed308a363fb2014ce58f9dd108aa827705182c
bpb2008 <brittanythompson08@gmail.com>
2024-05-02 09:56:31 -0500

    correct formatting for heading text

--- journeywise-app/src/components/homepage/Hero.jsx
+++ journeywise-app/src/components/homepage/Hero.jsx
@@ -6,14 +6,18 @@ const Hero = () => {
   return (
     <header id="hero" className="flex flex-row p-0 mt-80 ml-[138px]">
       <section id="heading-subheading">
-        <h2 className="text-[62px] font-['DM Serif Text'] w-[558px] h-[170px] not-italic font-normal leading-normal">
+        <div className="w-[450px] h-[170px]">
+        <h2 className="text-[62px] font-['DM Serif Text'] not-italic font-normal leading-normal">
           Finding Your Path & Story
         </h2>
-        <h6 className="text-[22px]w-[451px] h-[57px] mt-[21px] mr-[104px]">
+        </div>
+        <div className="w-[451px] h-[57px]">
+        <h6 className="text-[22px] mt-[21px] mr-[104px]">
           During this time of uncertainty, lets redefine how you tell
           your story.
         </h6>
-        <Buttons rounded primary className="w-[223px] h=[45px] text-[14px] px-[60px] py-[13px]">Let's Begin</Buttons>
+        </div>
+        <Buttons rounded primary className="w-[223px] h=[45px] text-[14px] px-[60px] py-[13px] mt-[30px]">LET'S BEGIN</Buttons>
       </section>
       <section id="collage" className="ml-[172.01px] mr-0">
         <Collage />
