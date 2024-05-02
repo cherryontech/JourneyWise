253354323fec0788dee4fece748928addbfd8849
bpb2008 <brittanythompson08@gmail.com>
2024-05-02 14:25:39 -0500

    created a new branch for homepage guide

---
+++ journeywise-app/src/components/homepage/HomepageGuide.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+const HomepageGuide = () => {
+  return (
+    <div>
+      <header>
+        <p>HERE'S HOW IT WORKS</p>
+        <h3>Welcome To An Interactive & Fun Process</h3>
+      </header>
+    </div>
+  )
+}
+
+export default HomepageGuide
--- journeywise-app/src/navigation/Index.jsx
+++ journeywise-app/src/navigation/Index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/homepage/Hero'
 import OurVision from '../components/homepage/OurVision'
+import HomepageGuide from '../components/homepage/HomepageGuide'
 
 const Index = () => {
   return (
     <>
       <Hero />
       <OurVision />
+      <HomepageGuide />
     </>
   )
 }
