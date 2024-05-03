ceac5a0e21faefe78e9759b0a97cb90e9112d0cf
bpb2008 <brittanythompson08@gmail.com>
2024-05-03 12:05:56 -0500

    set layout for homepage guide

--- journeywise-app/src/components/homepage/Collage.jsx
+++ journeywise-app/src/components/homepage/Collage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Collage = () => {
   return (
-    <div className="mt-[-80px]">
+    <div className="mt-[-200px] ml-[-80px]">
     <img 
     id="collage"
     src="/journeywise-hero.png" 
--- journeywise-app/src/components/homepage/HomepageGuide.jsx
+++ journeywise-app/src/components/homepage/HomepageGuide.jsx
@@ -4,22 +4,27 @@ const HomepageGuide = () => {
   return (
     <div className="mt-[100px] bg-[#E5F0F7]">
       <header className="ml-[138px] pt-[113px]">
-        <p className="text-[18px]">HERE'S HOW IT WORKS</p>
-        <h3 className="text-[48px]">Welcome To An Interactive & Fun Process</h3>
+        <p className="text-[18px] mb-[21px]">HERE'S HOW IT WORKS</p>
+        <h3 className="text-[48px] mr-[800px]">Welcome To An Interactive & Fun Process</h3>
       </header>
-      <section>
+      <section className="pb-[150px]">
         <div id="question-vectors" className="flex flex-row mt-[169.97px]">
           <img src="/Vector.svg" className="ml-[138px]" /> 
-          <img src="/Vector.svg" className="ml-[422.67px]" />
-          <img src="/Vector.svg" className="ml-[329.67px]" /> 
+          <img src="/Vector.svg" className="ml-[350px]" />
+          <img src="/Vector.svg" className="ml-[330px]" /> 
         </div>
-        <div className="flex flex-row">
-          <div className="ml-[138px] mt-[48.67px]">
-          <h4 className="text-[32px]">Discover Your Archetype</h4>
-          <p className="mt-[13px]">Are you The Perfectionist, The Expert, or The Soloist? Select the one that reflects your journey best.</p>
+        <div id="instructions" className="flex flex-row">
+          <div id="discover" className="ml-[138px] mt-[48.67px]">
+          <h4 className="text-[32px] mr-[100px]">Discover Your Archetype</h4>
+          <p className="mt-[13px] mr-[100px]">Are you The Perfectionist, The Expert, or The Soloist? Select the one that reflects your journey best.</p>
           </div>
-          <div>
-            <h4></h4>
+          <div id="engage" className="mt-[48.67px]" >
+            <h4 className="text-[32px] mr-[150px] ml-[50px]" >Engage With Our Assessment</h4>
+            <p className="mt-[13px] ml-[50px] mr-[200px]" >Answer a few questions to reflect on your journey. Remember, to be honest as that helps us curate results crafted exclusively for you.</p>
+          </div>
+          <div id="unlock" className="mt-[48.67px] mr-[144px] ml-[-100px]" >
+            <h4 className="text-[32px] mr-[100px]" >Unlock Your Results</h4>
+            <p className="mt-[13px]" >That's all there is to it! You are done. Use these results to redefine how you tell your story.</p>
           </div>
         </div>
       </section>
