9b0ed13f2579d96c465d94b757f033fa6550031f
bpb2008 <brittanythompson08@gmail.com>
2024-05-04 12:00:20 -0500

    fixed z indez issues with vectors

--- journeywise-app/src/components/homepage/OurVision.jsx
+++ journeywise-app/src/components/homepage/OurVision.jsx
@@ -6,9 +6,9 @@ const OurVision = () => {
     <img id="visionrectangle" src='/Rectangle 180.svg' alt="A person leaning over a table, holding a black marker in their hand in the process of drawing. " /> 
     <header>
       <p className="text-[18px] ml-[141px] mt-[172px] not-italic">OUR VISION</p>
-      <h3 className="text-[48px] ml-[141px] mt-[21px] mr-[20px] not-italic font-normal leading-normal">At JourneyWise, You're The Hero In Your Story</h3>
-      <p className="ml-[141px] mt-[21px] mr-[32px] text-[18px] not-italic font-normal leading-[35px]">You've been laid off, and that feeling hurts. It's a dark time where doubts have started to creep in, confidence is at an all-time low, and uncertainty is overwhelming. But that's where we come in to help! Let's redefine how you tell your story, empowering you to recognize and embrace the skills you've gained during your career break.</p>
-      <img id='vector' src='/Vector 177.svg' className="mt-[-650px] w-[1034.52px] h-[685.813px]" alt="A swirly decoration behind the text." /> 
+      <h3 className="text-[48px] ml-[141px] mt-[21px] mr-[20px] not-italic font-normal leading-normal relative z-1">At JourneyWise, You're The Hero In Your Story</h3>
+      <p className="ml-[141px] mt-[21px] mr-[32px] text-[18px] not-italic font-normal leading-[35px] relative z-1">You've been laid off, and that feeling hurts. It's a dark time where doubts have started to creep in, confidence is at an all-time low, and uncertainty is overwhelming. But that's where we come in to help! Let's redefine how you tell your story, empowering you to recognize and embrace the skills you've gained during your career break.</p>
+      <img id='vector' src='/Vector 177.svg' className="mt-[-650px] w-[1034.52px] h-[685.813px] relative z-[-2]" alt="A swirly decoration behind the text." /> 
     </header>
     </div>
   )
