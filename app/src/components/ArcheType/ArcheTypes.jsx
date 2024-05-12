2092f725c505bd8c7be91e56f747b6445725d6a8
Mahlet <mahitimulu@gmail.com>
2024-05-12 20:31:13 +0200

    added the data.json file and the dot functionality

--- app/src/components/ArcheType/ArcheTypes.jsx
+++ app/src/components/ArcheType/ArcheTypes.jsx
@@ -1,15 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import Img from "../../assets/ArchetypeBg.png";
 import ShowArcheType from "./ShowArcheType";
+import data from "../../../Data/data.json"
 
 function ArcheTypes() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleDotClick = (nextIndex) => {
+    setActiveIndex((currentIndex)=>{
+      if(currentIndex=== nextIndex){
+        return 0;
+      }
+      else{
+        return nextIndex
+      }
+    });
+  };
+  const render = data.archetype.map((el, index)=>{
+    return (
+      <ShowArcheType className="flex"  key = {el.id}  title = {el.title}text= {el.text} img= {el.image} 
+      isActive={index ===activeIndex} />
+    )
+  })
+
+  
+
+  const renderDotSection = () => {
+    return (
+      <div className="flex justify-center mt-4">
+        {data.archetype.map((el, index) => (
+          <button
+            key={index}
+            className={`mx-1 w-3 h-3 rounded-full bg-gray-500 cursor-pointer ${
+              activeIndex === index ? "bg-gray-900" : ""
+            }`}
+            onClick={() => handleDotClick(index)}
+          ></button>
+        ))}
+      </div>
+    );
+  };
+  
+
   return (
-    <section className="h-screen">
+    <section>
       <div
-        className="bg-cover bg-center h-full bg-no-repeat"
+        className="bg-cover bg-center h-full  w-fullbg-no-repeat"
         style={{ backgroundImage: `url(${Img})` }}
       >
-        <ShowArcheType />
+      <div className = "flex ">
+       {render}
+       </div>
+       {renderDotSection()}
       </div>
     </section>
   );
--- app/src/components/ArcheType/ShowArcheType.jsx
+++ app/src/components/ArcheType/ShowArcheType.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import SVGdata from "../../assets/archetype.svg";
 
-function ShowArcheType() {
+function ShowArcheType({title, text, img, isActive}) {
   return (
-    <figure className= "pt-[77px] pb-[89px] pl-[42px] pr-[39px] ">
+
+    <figure className={`${
+        isActive ? "block" : "hidden"
+      } pt-[77px] pb-[89px] pl-[42px] pr-[39px] `}>
       <div className="min-w-[294px] min-h-[566px] bg-white rounded-[22px] flex flex-col items-center ">
-        <img  className = "mt-[9px] mb-[32px] ml-[18px] mr-[44px]"src={SVGdata} alt="pic" />
-        <div>
-          <h2>Title</h2>
+        <img  className = "mt-[9px] mb-[33px] ml-[18px] mr-[44px]"src={img} alt="pic" />
+        <div className = " flex-1  min-w-[294px] min-h [287px] rounded-[22px] bg-[#F5FF82] text-center">
+          <h2 className = "text-[#292929)] font-[DM Serif Text] text-[30px] font-normal leading-normal mt-[20px] mb-[8px] ml-[23px] mr-[29px]">{title}</h2>
+        <p className = "text-base text-[#2C2B2B] ffont-sans  text-[16px] font-normal leading-[2.7] ml-[23px] mr-[21px] mb-[27px]">{text}
+           </p>
         </div>
       </div>
     </figure>
