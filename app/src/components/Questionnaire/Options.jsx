9ea1b8d971c37f3407cdec8b7f343f102e179216
Mahlet <mahitimulu@gmail.com>
2024-05-18 01:52:56 +0200

    added the logic for the softskill calcuation

--- app/src/components/Questionnaire/Options.jsx
+++ app/src/components/Questionnaire/Options.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 function Options ({answerChoices, handleSelected}){
-    const handleChange = (i)=>{
-        handleSelected(i)
+    const handleChange = (i,id)=>{
+        handleSelected(i, id)
     }
 
     return (
         <div>
             {answerChoices.map((choice, i) => (
-        <div  key = {choice} id="question" className="flex flex-col">
+        <div  key = {choice.answer} id="question" className="flex flex-col">
         <div className="border border-[#7A7A7A] rounded-[13px] w-[486px] h-[60px] py-[12px] pl-[14px] mb-[12px]">
-          <input type="radio" id="perfectionist" name="archetype" value="The Perfectionist" className="mr-[20px] w-[20px] h-[20px]"onChange = {()=>handleChange(i)} />
-          <label>{choice}</label>
+          <input type="radio" id="perfectionist" name="archetype" value="The Perfectionist" className="mr-[20px] w-[20px] h-[20px]"onChange = {()=>handleChange(i, choice.id)} />
+          <label>{choice.answer}</label>
         </div>
         </div>
       ))
--- app/src/components/Questionnaire/Questionnaire.jsx
+++ app/src/components/Questionnaire/Questionnaire.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Buttons from '../Buttons/Buttons'
 import Options from './Options'
 import ProgressBar from './ProgressBar'
@@ -10,8 +10,21 @@ const Questionnaire = () => {
   
   const [index , setIndex ]= useState(0)
   const [selected, setSelected] = useState(null);
+  const [selectedId , setSelectedId ]= useState(null)
+  const [count, setCount] = useState({})
 
-  console.log(selected)
+  useEffect(() => {
+    if (selected !== null) {
+      const selectedOption = questions.questions[index].options[selected];
+      const newCount = { ...count };
+      selectedOption.softSkills.forEach(skill => {
+        newCount[skill] = (newCount[skill] || 0) + 1;
+      });
+      setCount(newCount);
+    }
+  }, [selected, index]);
+
+  
    
   const numberQuestion = index+1 
 
@@ -19,6 +32,7 @@ const Questionnaire = () => {
     if (index < questions.questions.length - 1) {
       setIndex(index + 1);
       setSelected(null)
+      setSelectedId(null)
     }
   }
   const handleBack = ()=>{
@@ -27,8 +41,9 @@ const Questionnaire = () => {
     }
   }
 
-  const handleSelected = (i)=>{
+  const handleSelected = (i, id)=>{
     setSelected(i)
+    setSelectedId(id)
   } 
 
   return (
@@ -36,12 +51,12 @@ const Questionnaire = () => {
       <ProgressBar />
       <header>
         <p className="text-[18px]">QUESTION{ numberQuestion}/{questions.questions.length}</p>
-        <h3 className="mt-[21px] mb-[56px] text-[48px]">{questions.questions[index].question}</h3>
+        <h3 className="mt-[21px] mb-[56px] text-[48px]">{(questions.questions[index].question)}</h3>
       </header>
-      <Options answerChoices={questions.questions[index].answerChoices} handleSelected = {handleSelected}/>
+      <Options answerChoices={questions.questions[index].options} handleSelected = {handleSelected}/>
       <div className="ml-[200px] pb-[0px] mt-[103px]">
         <Buttons  onClick = {handleBack}primary rounded  alternate={index === 0} className="mr-[50px] px-[33px] py-[17px]" >BACK</Buttons>
-        <Buttons onClick = {handleNext} alternate = {selected === null} primary = {selected !=null}rounded  className="px-[33px] py-[17px]" >NEXT</Buttons>
+        <Buttons onClick = {handleNext} alternate = {selectedId === null} primary = {selectedId !=null}rounded  className="px-[33px] py-[17px]" >NEXT</Buttons>
       </div>
     </div>
   )
