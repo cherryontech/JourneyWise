dc3a8dba93204cb12baa9fac7bbb8e48480551ac
Mahlet <mahitimulu@gmail.com>
2024-05-17 22:58:01 +0200

    updated the functionality for the buttons & refactored the options

--- app/src/components/ArcheTypes/StartQuestionnaire.jsx
+++ app/src/components/ArcheTypes/StartQuestionnaire.jsx
@@ -4,13 +4,13 @@ import LearnArchetypes from "../Questionnaire/LearnArchetypes"
 import Footer from '../Footer/Footer'
 
 const StartQuestionnaire = () => {
-    const [currentQuestion, setCurrentQuestion] = useState(0)
+   
 
     return (
         <div className="h-screen">
         <div className="flex flex-row ml-[655px]">
             <LearnArchetypes />
-            <Questionnaire currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion} />
+            <Questionnaire />
         </div>
         <Footer /> 
         </div>
--- app/src/components/Questionnaire/Questionnaire.jsx
+++ app/src/components/Questionnaire/Questionnaire.jsx
@@ -1,38 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Buttons from '../Buttons/Buttons'
+import Options from './Options'
 import ProgressBar from './ProgressBar'
 import { questionData } from '../../Data/questionData'
 
 
-const Questionnaire = ({currentQuestion, setCurrentQuestion, handleNextQuestion}) => {
+const Questionnaire = () => {
+  const [questions , setQuestions] = useState( questionData )
+  
+  const [index , setIndex ]= useState(0)
+  const [selected, setSelected] = useState(null);
 
-  const { questions } = questionData 
-  const { number, question, answerChoices } = questions[currentQuestion]
+  console.log(selected)
+   
+  const numberQuestion = index+1 
 
-  const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questions.length) {
-      setCurrentQuestion(nextQuestion);
+  const handleNext = () => {
+    if (index < questions.questions.length - 1) {
+      setIndex(index + 1);
+      setSelected(null)
     }
+  }
+  const handleBack = ()=>{
+    if (index > 0 ) {
+      setIndex(index -1);
+    }
+  }
+
+  const handleSelected = (i)=>{
+    setSelected(i)
+  } 
 
   return (
     <div className="bg-[#E5F0F7] lg:w-[779px] px-[25px]">
       <ProgressBar />
       <header>
-        <p className="text-[18px]">QUESTION {number}/8</p>
-        <h3 className="mt-[21px] mb-[56px] text-[48px]">{question}</h3>
+        <p className="text-[18px]">QUESTION{ numberQuestion}/{questions.questions.length}</p>
+        <h3 className="mt-[21px] mb-[56px] text-[48px]">{questions.questions[index].question}</h3>
       </header>
-      {answerChoices.map((choice) => (
-        <div id="question" className="flex flex-col">
-        <div className="border border-[#7A7A7A] rounded-[13px] w-[486px] h-[60px] py-[12px] pl-[14px] mb-[12px]">
-          <input type="radio" id="perfectionist" name="archetype" value="The Perfectionist" className="mr-[20px] w-[20px] h-[20px]" />
-          <label>{choice}</label>
-        </div>
-        </div>
-      ))
-}
+      <Options answerChoices={questions.questions[index].answerChoices} handleSelected = {handleSelected}/>
       <div className="ml-[200px] pb-[0px] mt-[103px]">
-        <Buttons primary rounded className="mr-[50px] px-[33px] py-[17px]" >BACK</Buttons>
-        <Buttons onClick={nextQuestion} alternate rounded className="px-[33px] py-[17px]" >NEXT</Buttons>
+        <Buttons  onClick = {handleBack}primary rounded  alternate={index === 0} className="mr-[50px] px-[33px] py-[17px]" >BACK</Buttons>
+        <Buttons onClick = {handleNext} alternate = {selected === null} primary = {selected !=null}rounded  className="px-[33px] py-[17px]" >NEXT</Buttons>
       </div>
     </div>
   )
