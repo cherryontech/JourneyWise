e8c92ca160ebd650b8728b9e4989b1f06358c894
Mahlet <mahitimulu@gmail.com>
2024-05-20 11:49:46 +0200

    added the desktop version

--- app/src/components/Result/Content.jsx
+++ app/src/components/Result/Content.jsx
@@ -10,24 +10,29 @@ function Content (){
                 <h2 className=" text-[36px] capitalize">Swipe left to see your evolution</h2>
                 <h3 className="font-features font-serifText text-[30px] mt-[21px] mb-[40px] ml-[16px] mr[26px] ">We think you're: </h3>
             </div>
-            <figure className = "min-w-[295px] min-h-[570px] rounded-[22px] bg-white shadow-lg ">
-                <div className = "flex flex-col justify-center items-center">
-                    <img src={Img} alt="perfections" />
-                <div className = "text-blackCustom text-center font-features font-serifTex font-normal leading-normal min-w-[295px] min-h-[336px] bg-[#F5FF82]">
-                    <h3 className = "text-[30px] mt-[84px] mb-[8px]" >The Pioneer</h3>
-                    <p className = " leading-[27px] min-w-[250px] min-h-[217px] mr-[21px] ml-[23px] mb-[35px]">  laorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis viverra tempor. 
+            <figure className = "min-w-[295px] min-h-[570px] rounded-[22px] bg-white shadow-lg md:max-w-[1142px] md:min-h-[518px] md:rounded-[29px] md:shadow-[9px_9px_12px_0px_rgba(0,0,0,0.15)] md:mx-[149px]">
+                <div className = "flex flex-col justify-center items-center md:flex-row md:justify-between">
+                <div className="flex justify-center items-center md:flex-1">
+                        <img className="min-w-[233px] min-h-[239px] pt-[9px] pb-[32px] pl-[18px] pr-[43px] md:min-w[340px] md:min-h-[308px]" src={Img} alt="pioneer" />
+                    </div>
+                
+                <div className = "text-blackCustom text-center font-features font-serifTex font-normal leading-normal min-w-[295px] min-h-[336px] bg-[#F5FF82] md:min-w-[574px] md:min-h-[519px] flex:1">
+                    <h3 className = "text-[30px] mt-[84px] mb-[8px] md:min-w-[402px] md:text-[48px] md:mt[126px] md:ml-[59px] md:mr-[112px] md:mb-[27px]" >The Pioneer</h3>
+                    <p className = " leading-[27px] min-w-[250px] min-h-[217px] mr-[21px] ml-[23px] mb-[35px] md:w-[432px]">  laorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis viverra tempor. 
                         Vivamus laoreet varius tellus, quis dapibus ligula. 
                         ed vitae metus sed tellus finibus pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. A</p>
                 </div>
                 </div>
-                
             </figure>
 
-            <div>
-                <figure>
-                    <img src="" alt="" />
-                </figure>
-            </div>
+            <button className="w-[204px] h-[45px] flex-shrink-0 rounded-[55px] border border-black mt-5 mx-auto flex items-center justify-center space-x-2">
+                <span className="w-[112px] h-[37px] flex-shrink-0 rounded-[55px] border border-black bg-black text-white flex items-center justify-center">
+                    I am
+                </span>
+                <span className="w-[87px] text-black text-center font-dmSans text-[14px] font-bold leading-normal tracking-[2px] uppercase">
+                    I was
+                </span>
+            </button>
         </section>
 
     )
