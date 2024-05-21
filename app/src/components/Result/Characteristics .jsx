a5192ec6f7ffc3e6cffb5a6b97ddfd0ca784f96b
Mahlet <mahitimulu@gmail.com>
2024-05-21 11:13:02 +0200

    added the characterstics section

--- app/src/components/Result/Characteristics .jsx
+++ app/src/components/Result/Characteristics .jsx
@@ -1,13 +1,67 @@
 import React from "react";
+import Char from "../../assets/result/characterstics.svg"
 
 function Characteristics (){
 
     return(
 
         <>
-        <div>
-        Characteristics 
-        </div>
+        <section className = "font-serifTex">
+            <h3 className = "min-w-[335px] text-[36px] captalize mb-[30px] md:min-w-[1138px] md:mt-[103px] md:mb-[56px] md:ml-[153px] md:mr-[149px] text-center md:text-[48px]">Your Super Powers</h3>
+        <figure className = "min-w-[295px] min-h-[415px] md:min-w-[450px] md:min-h-[497px] ">
+        <div className="grid grid-cols-1 md:grid-cols-3  ">
+            <div className = " bg-[#F5FF82] mb-[25px] rounded-[29px] shadow-[9px_9px_12px_0px_rgba(0,0,0,0.15) md:mb-[0] mr-[23px]">
+                
+            <header className = " pt-[25px] pl-[23px] pr-[234px] pb-[45px]  md:pt-[40px] md:pl-[40px] md:pr-[234px] md:pb-[61px]">
+                <img src={Char} alt="light bulb" className = "min-w-[37px] min-h-[60px] md:min-w-[51px] md:min-h-[80px]" />
+            </header>
+
+            <div>
+                <h2 className = "min-w-[210px] min-h-[38px] capitalize text-[30px] mr-[62px] ml-[23px] md:min-w-[282px] md:min-h-[52px] md:mr-[126px] md:ml-[42px] md:tex-[32px] ">
+                Perfectionism
+                </h2>
+                <p className = "font-sans leading-[27px] mr-[11px] ml-[23px] mt-[15px] mb-[76px] md:min-w-[351px] md:min-h-[209px] md:text-[18px] md:leading-[35px] md:mt-[14px] md:mr-[57px] md:ml-[42px] md:mb-[40px]">
+                rem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ultricies felis. Pellentesque ut dapibus leo. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ultricies felis. Pellentesque ut dapibus leo.   
+                </p> </div>
+            
+            </div>
+            <div className = " bg-[#F5FF82] mb-[25px] rounded-[29px] shadow-[9px_9px_12px_0px_rgba(0,0,0,0.15) md:mb-[0] mr-[23px]">
+                
+            <header className = " pt-[25px] pl-[23px] pr-[234px] pb-[45px]  md:pt-[40px] md:pl-[40px] md:pr-[234px] md:pb-[61px]">
+                <img src={Char} alt="light bulb" className = "min-w-[37px] min-h-[60px] md:min-w-[51px] md:min-h-[80px]" />
+            </header>
+
+            <div>
+                <h2 className = "min-w-[210px] min-h-[38px] capitalize text-[30px] mr-[62px] ml-[23px] md:min-w-[282px] md:min-h-[52px] md:mr-[126px] md:ml-[42px] md:tex-[32px] ">
+                Perfectionism
+                </h2>
+                <p className = "font-sans leading-[27px] mr-[11px] ml-[23px] mt-[15px] mb-[76px] md:min-w-[351px] md:min-h-[209px] md:text-[18px] md:leading-[35px] md:mt-[14px] md:mr-[57px] md:ml-[42px] md:mb-[40px]">
+                rem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ultricies felis. Pellentesque ut dapibus leo. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ultricies felis. Pellentesque ut dapibus leo.   
+                </p> </div>
+            
+            </div>
+            <div className = " bg-[#F5FF82] mb-[25px] rounded-[29px] shadow-[9px_9px_12px_0px_rgba(0,0,0,0.15) md:mb-[0] mr-[23px]">
+                
+            <header className = " pt-[25px] pl-[23px] pr-[234px] pb-[45px]  md:pt-[40px] md:pl-[40px] md:pr-[234px] md:pb-[61px]">
+                <img src={Char} alt="light bulb" className = "min-w-[37px] min-h-[60px] md:min-w-[51px] md:min-h-[80px]" />
+            </header>
+
+            <div>
+                <h2 className = "min-w-[210px] min-h-[38px] capitalize text-[30px] mr-[62px] ml-[23px] md:min-w-[282px] md:min-h-[52px] md:mr-[126px] md:ml-[42px] md:tex-[32px] ">
+                Perfectionism
+                </h2>
+                <p className = "font-sans leading-[27px] mr-[11px] ml-[23px] mt-[15px] mb-[76px] md:min-w-[351px] md:min-h-[209px] md:text-[18px] md:leading-[35px] md:mt-[14px] md:mr-[57px] md:ml-[42px] md:mb-[40px]">
+                rem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ultricies felis. Pellentesque ut dapibus leo. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ultricies felis. Pellentesque ut dapibus leo.   
+                </p> </div>
+            
+            </div>
+            </div>
+        </figure>
+        
+        </section>
         </>
     )
 }
