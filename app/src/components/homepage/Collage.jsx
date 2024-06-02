386768b39ad4c59e79af48b5833786d35233dec1
bpb2008 <brittanythompson08@gmail.com>
2024-06-02 11:52:02 -0500

    fixing responsiveness on homepage, hero section specifically

--- app/src/components/homepage/Collage.jsx
+++ app/src/components/homepage/Collage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import './collage-custom.css'
 
 const Collage = () => {
   return (
-    <div className="lg:mt-[-100px] sm:mr-[50px]">
+    <div className="collage-custom">
     <img 
     id="collage"
     src="/journeywise-hero.png" 
-    className="sm:w-[330px] sm:h-[369px] lg:w-[702px] lg:h-[753px]"
     alt="A photo collage comprised of nine different graphics and designs, including a gray arrow, a hand holding a marker preparing to write, a person sitting in front of a laptop, the JourneyWise mascot--a blue and yellow star like creature with white arms and legs, and a pen."
     />
     </div>
--- app/src/components/homepage/Hero.jsx
+++ app/src/components/homepage/Hero.jsx
@@ -1,36 +1,37 @@
 import React from 'react'
 import Collage from './Collage'
 import Buttons from '../Buttons/Buttons'
+import Vectors from './Vectors'
 import useShowSecNotice from '../../Hook/useShowSecNotice'
+import './hero-custom.css'
 
 
 
 const Hero = () => {
-     const [handleClick , ShowSecNotice] = useShowSecNotice();
+  const [handleClick , ShowSecNotice] = useShowSecNotice();
   return (
-    <header id="hero" className="flex lg:flex-row sm:flex-col-reverse lg:mt-[140px] sm:mt-[50px] lg:ml-[138px]">
-      <section id="heading-subheading" className="lg:ml-[138px] sm:my-[50px]">
-        <div className="lg:w-[450px] lg:h-[170px] sm:w-[305px] sm:h-[142px]">
-        <h2 className="text-[64px] font-['DM Serif Text'] not-italic font-normal leading-normal relative">
+    <header className="hero-custom">
+      <section id="heading-subheading">
+        <div>
+          <h2>
           Finding Your Path & Story
-        </h2>
-        <img src="/Vector 921.svg" className="lg:ml-[-150px] lg:mt-[-250px] sm:mt-[-200px]" alt="A swirly decoration behind the text." /> 
-        <img src="/Vector 922.svg" className="ml-[345px] mt-[-295px]" alt="A line decoration next to the text." />
-        <img src="/Vector 923.svg" className="ml-[380px] mt-[-30px]" alt="A line decoration next to the text." />
-        <img src="/Vector 924.svg" className="ml-[400px] mt-[20px]" alt="A line decoration next to the text." />
+          </h2>
+          <div>
+            <Vectors /> 
+          </div>
         </div>
-        <div className="w-[451px] h-[57px]">
-        <h6 className="text-[22px] mt-[21px] mr-[104px]">
+        <div id="subheading">
+        <h6>
           During this time of uncertainty, lets redefine how you tell
           your story.
         </h6>
-        <img src="/Vector 926.svg" className="ml-[-55px] mt-[50px]" alt="A line decoration behind the button." /> 
+        <img src="/Vector 926.svg" className="ml-[-55px] mt-[25px]" alt="A line decoration behind the button." /> 
         <img src="/Vector 927.svg" className="ml-[40px] mt-[-5px]" alt="A line decoration below the button." /> 
         </div>
-        <Buttons rounded primary className="w-[223px] h=[45px] text-[14px] px-[60px] py-[13px] mt-[30px]" onClick= {handleClick}>LET'S BEGIN</Buttons>
+        <Buttons id="lets-begin" rounded primary className="w-[223px] h=[45px] text-[14px] px-[60px] py-[13px] mt-[30px]" onClick= {handleClick}>LET'S BEGIN</Buttons>
         <ShowSecNotice/>
       </section>
-      <section id="collage" className="sm:mt-[50px] lg:ml-[172.01px] lg:mr-0">
+      <section id="collage">
         <Collage />
       </section>
     </header>
