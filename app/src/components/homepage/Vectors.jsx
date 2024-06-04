175b3b9090dbeec634182eaf6bddfff9cfa8abe3
bpb2008 <brittanythompson08@gmail.com>
2024-06-04 12:16:50 -0500

    adjusted line heights on homepage

--- app/src/components/homepage/Vectors.jsx
+++ app/src/components/homepage/Vectors.jsx
@@ -5,9 +5,9 @@ const Vectors = () => {
   return (
     <>
     <img id="swirl1" src="/Vector 921.svg" className="vectors-custom" alt="A swirly decoration behind the text." /> 
-    <img id="line1" src="/Vector 922.svg" className="vectors-custom sm: hidden" alt="A line decoration next to the text." />
-    <img id="line2" src="/Vector 923.svg" className="vectors-custom sm: hidden" alt="A line decoration next to the text." />
-    <img id="line3" src="/Vector 924.svg" className="vectors-custom sm: hidden" alt="A line decoration next to the text." />
+    <img id="line1" src="/Vector 922.svg" className="vectors-custom" alt="A line decoration next to the text." />
+    <img id="line2" src="/Vector 923.svg" className="vectors-custom" alt="A line decoration next to the text." />
+    <img id="line3" src="/Vector 924.svg" className="vectors-custom" alt="A line decoration next to the text." />
     </>
   )
 }
