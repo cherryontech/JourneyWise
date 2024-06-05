6390c925815aaa215f436cd59354defa65eb8abc
bpb2008 <brittanythompson08@gmail.com>
2024-06-04 20:08:29 -0500

    swapped out Notion social media image with a Cherry image

--- app/src/components/Footer/Footer.jsx
+++ app/src/components/Footer/Footer.jsx
@@ -17,8 +17,8 @@ const Footer = () => {
             <a href="https://www.linkedin.com/company/cherry-on-tech/posts/?feedView=all">
               <li className ={style("listStyle")}><img src="/LinkedIn-Icon-PH.svg" alt="LinkedIn Logo, link to Cherry on Tech's LinkedIn" /></li>
             </a>
-            <a href="https://www.notion.so/cherryontech-org/Spring-2024-Cohort-Workspace-8468841576554899ac234d5c00a3a500">
-              <li className ={style("listStyle")}><img src="Notion-Icon-PH.svg" alt="Notion Logo, link to Cherry on Tech's Notion" /></li>
+            <a href="https://cherryon.tech/">
+              <li className ={style("listStyle")}><img src="Cherry-Icon-PH.svg" alt="Cherry Logo, link to Cherry on Tech's Website" /></li>
             </a>
             <a href="https://www.youtube.com/@cherryontech">
               <li className ={style("listStyle")}><img src="Youtube-Icon-PH.svg" alt="Youtube Logo, link to Cherry on Tech's Youtube Channel" /></li>
