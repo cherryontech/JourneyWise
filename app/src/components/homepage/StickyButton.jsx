0a6dc262dc85c5dc962d0deff35c46973031743c
bpb2008 <brittanythompson08@gmail.com>
2024-06-10 22:15:08 -0500

    fixed margins and padding on homepage to get rid of white space

--- app/src/components/homepage/StickyButton.jsx
+++ app/src/components/homepage/StickyButton.jsx
@@ -1,10 +1,14 @@
 import React from 'react'; 
-import './sticky-button.css'
+import './sticky-button.css'; 
+import useShowSecNotice from '../../Hook/useShowSecNotice';
 
 const StickyButton = () => {
+  const [handleSecNotice , ShowSecNotice] = useShowSecNotice();
+
   return (
     <div id="stickycontainer" className="sticky-button">
-      <button id="stickybutton">BEGIN JOURNEY</button>
+      <button id="stickybutton" onClick={handleSecNotice} >BEGIN JOURNEY</button>
+      <ShowSecNotice /> 
     </div>
   )
 }
