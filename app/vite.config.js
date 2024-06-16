99b66564616b735a075aecc16b9de1e37cb8ea77
Mahlet <mahitimulu@gmail.com>
2024-06-16 22:58:11 +0200

    updating the correct htm2canvas setup

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['emailjs-com', 'html2canvas', 'jspdf'], // Specify external dependencies here
+      external: [
+        'emailjs-com',
+        /^html2canvas($|\/)/,  // Use regex to match html2canvas and its submodules
+        'jspdf',
+      ],
     },
   },
 });
