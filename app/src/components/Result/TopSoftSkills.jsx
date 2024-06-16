f74ac6210e77561e04712e3b11548c64d9cc69ea
bpb2008 <brittanythompson08@gmail.com>
2024-06-15 22:16:01 -0500

    styled top soft skills component

--- app/src/components/Result/TopSoftSkills.jsx
+++ app/src/components/Result/TopSoftSkills.jsx
@@ -4,17 +4,13 @@ import goals from "../../assets/topSkills/goals.svg"
 import lightbulb from "../../assets/topSkills/lightbulb.svg"
 import gear from "../../assets/topSkills/gear.svg"
 import { Check } from 'lucide-react';
-<<<<<<< HEAD
-function TopSoftSkills(){
-   const [active, setActive] = useState("TopSkills")
-=======
 import './topSoftSkills-custom.css'
 
-
 const TopSoftSkills = () => {
->>>>>>> b273192 (committing to rebase)
+   
     const location = useLocation();
     const {arcTypeCount:{skills}}= location.state
+    const [active, setActive] = useState("TopSkills")
 
     const handleClick = (skills)=>{
         setActive(skills)
@@ -28,30 +24,29 @@ const TopSoftSkills = () => {
             <div>
                     <div>
                         <div id="topHeader">
-                         <h2>Your Top Soft Skills</h2>
-                         <h3 className = "text-[30px] text-black text-center">  
-                         Toggle Below to explore your Unique Profile
-                         </h3>
+                         <h3>Your Top Soft Skills</h3>
+                         <h4>Toggle Below To Explore Your Unique Profile</h4>
                         </div>
-                        <div className="mt-[30px] flex justify-center items-center border border-black rounded-[36px]">
-                                <button className={`relative min-w-[100px] min-h-[45px] text-center text-[14px] font-bold tracking-wide uppercase ${active === "TopSkills" && "bg-black text-white border rounded-full"}`} onClick = {()=>handleClick("TopSkills")}>
+                        <div id="togglebutton">
+                                <button id="skills" className={`${active === "TopSkills" && "bg-black text-white"}`} onClick = {()=>handleClick("TopSkills")}>
                                     TOP SKILLS
                                 </button>
-                                <button className={`relative min-w-[100px] min-h-[45px] mx-[16px] text-center text-[14px] font-bold tracking-wide uppercase ${active === "Strengths" &&  "bg-black text-white border rounded-full"}`} onClick = {()=>handleClick("Strengths")}>
+                                <button id="strengths" className={`${active === "Strengths" &&  "bg-black text-white"}`} onClick = {()=>handleClick("Strengths")}>
                                     STRENGTHS
                                 </button>
-                                <button className={`relative min-w-[100px] min-h-[45px] text-center text-[14px] font-bold tracking-wide uppercase ${active === "Goals" && "bg-black text-white border rounded-full"}`} onClick = {()=>handleClick("Goals")}>
+                                <button id="goals" className={`${active === "Goals" && "bg-black text-white"}`} onClick = {()=>handleClick("Goals")}>
                                     GOALS
                                 </button>
                             
-                            </div>
+                        </div>
                     </div>
+                    <div id="cardcontainer">
                     {
 
                     Object.keys(skills).map((el, i)=>(
 
 
-                    <figure key = {el} className ={ `mt-[56px] w-[295px] h-[472px] mx-[40px] border-29px shadow-[9px_9px_12px_0_rgba(0,0,0,0.15)] rounded ${i=== 0 && "bg-white"} ${i===1 && "bg-[#F5FF82]"} ${i=== 2 && "bg-[#2C2B2B]"}`} >
+                    <figure id="skillcards" key = {el} className ={ ` shadow-[9px_9px_12px_0_rgba(0,0,0,0.15)] ${i=== 0 && "bg-white"} ${i===1 && "bg-[#F5FF82]"} ${i=== 2 && "bg-[#2C2B2B]"}`} >
                     <div>
                        <div>
                     <img  className ={`fill-current text-black ${active === "TopSkills" && i === 2 ? "fill-white" : "fill-black"} ${active ==="Strengths" && i === 2 ? "fill-white" : "fill-black"} ${active ==="goals" && i !== 2 ? "fill-black" :  "fill-white" }`}src={(active ==="TopSkills" && lightbulb) || (active === "Strengths" && gear) || (active === "Goals" && goals)} alt="goals" />
@@ -91,6 +86,7 @@ const TopSoftSkills = () => {
 
              
                     }
+                    </div>
             </div>
         </section>
         
