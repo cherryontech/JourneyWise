d76f8222219e026424fad7f998726f2741fa382f
bpb2008 <brittanythompson08@gmail.com>
2024-06-20 22:16:50 -0500

    swapped out email confirmation text

--- app/src/components/Result/EmailConfirmation.jsx
+++ app/src/components/Result/EmailConfirmation.jsx
@@ -6,7 +6,7 @@ const EmailConfirmation = () => {
     <div id="confirmationcontainer" className="emailConfirmation-custom">
       <img src="journeywise-icon-checkmark.svg" /> 
       <h3>We've Sent Your Results</h3>
-      <p>We’ve sent the results to you. Make sure to keep your results in a safe spot and to review before any upcoming opportunities!</p>
+      <p>We've sent the PDF of your results to the email you provided. Make sure to keep your results in a safe spot and to review before any upcoming opportunities!</p>
     </div>
   ) 
 }
