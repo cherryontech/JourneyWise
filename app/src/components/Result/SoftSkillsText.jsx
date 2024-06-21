17a68d50fb91a5f7b5dd9f46288d557acbd28aa6
bpb2008 <brittanythompson08@gmail.com>
2024-06-21 09:15:04 -0500

    edited text in soft skills

--- app/src/components/Result/SoftSkillsText.jsx
+++ app/src/components/Result/SoftSkillsText.jsx
@@ -10,7 +10,7 @@ const SoftSkillsText = () => {
                 <h3>Soft Skills All Around</h3>
             </div>
             <p id="skillsdescription">
-            After analyzing your results, we have identified the three top skills that resonate most with you as an individual. These skills complement your archetype and provide a deeper understanding of your unique and impressive skill set. Recognizing these strengths can empower you to leverage them effectively in your personal and professional life.
+            After analyzing your results, we have identified the six soft skills that resonate most with you as an individual. These skills complement your archetype and provide a deeper understanding of your unique and impressive skill set. Recognizing these strengths can empower you to leverage them effectively in your personal and professional life.
             </p>
         </section>
     )
