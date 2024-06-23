6ca4b050f6ea22d1eccbb8f05b5dfd5ceb5eabbf
Mahlet <mahitimulu@gmail.com>
2024-06-23 01:56:08 -0400

    updated the check color for the top skill

--- app/src/components/Result/TopSoftSkillsText.jsx
+++ app/src/components/Result/TopSoftSkillsText.jsx
@@ -10,7 +10,7 @@ const TopSkillsText = ({texts, index}) => {
         {
         texts.map((el, i)=>(
          <div key = {i} className = "flex gap-[5px]">
-                                <span className ="inline-block">{<Check className ={`border rounded-full bg-black text-white ${index===2 &&  " text-[#000] bg-white"}`}/>}</span>
+                                <span className ="inline-block">{<Check className ={`border rounded-full bg-black text-white ${index===2 &&  " text-[#111] bg-white"}`}/>}</span>
                                 <p className ={`${index===2 && "text-white"}`}>{el}</p>
                             </div>
 ))}
